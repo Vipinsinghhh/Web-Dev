a9e0d07842bd53cf09bb5acff2199dd808c2c35b
Vipinsinghhh <169113380+Vipinsinghhh@users.noreply.github.com>
2025-08-21 00:37:04 +0530

    comparison in datatypes in javascript

---
+++ Javascript/Basics/comparison.js
@@ -0,0 +1,23 @@
+//comparison in datatypes in javascript
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
