529cb0f82fb764571b0277a1107edc591ac79314
Vipinsinghhh <169113380+Vipinsinghhh@users.noreply.github.com>
2025-09-12 10:32:38 +0530

    Immediately Invoked Function Expressions(IIFE)

---
+++ Javascript/Basics3/IIFE.js
@@ -0,0 +1,25 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+
+function chai(){     //this is named IIFE
+    console.log(`DB CONNECTED `);
+    
+}
+
+chai();
+
+
+( function aurcode(){    
+    console.log(`DB CONNECTED 2`)
+})();    //must use semicolon for ending the context of function otherwise next function doesn't work
+
+
+( () => {           //this is simple or unnamed IIFE
+    console.log(`DB CONNECTED 3`)
+})();
+
+
+// This is how argument passed in the iife function:-
+( (name) => {
+    console.log(`DB CONNECTED 4 ${name}`)
+})("vipin");
