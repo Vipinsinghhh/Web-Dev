6ce3a8bedf13060793b2b937e669f1aaeac7da31
Vipinsinghhh <169113380+Vipinsinghhh@users.noreply.github.com>
2025-10-25 21:46:30 +0530

    Project 1 - colorChanger

---
+++ Javascript/Projects/project-1_colorChanger/script.js
@@ -0,0 +1,28 @@
+const body = document.querySelector('body');
+const buttons = document.querySelectorAll('.button');
+
+buttons.forEach(function (button){
+    console.log(button);
+
+    button.addEventListener('click',function(event){
+        console.log(event);
+        console.log(event.target);
+
+        if(event.target.id === 'grey'){
+            body.style.backgroundColor = event.target.id;
+        };
+        if(event.target.id === 'white'){
+            body.style.backgroundColor = event.target.id;
+        };
+        if(event.target.id === 'blue'){
+            body.style.backgroundColor = event.target.id;
+        };
+        if(event.target.id === 'yellow'){
+            body.style.backgroundColor = event.target.id;
+        };
+        if(event.target.id === 'purple'){
+            body.style.backgroundColor = event.target.id;
+        };
+         
+    })
+})
