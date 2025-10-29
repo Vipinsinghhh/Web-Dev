998de12491a4d9b84fc653412f1ba48515b315f2
Vipinsinghhh <169113380+Vipinsinghhh@users.noreply.github.com>
2025-10-30 01:20:23 +0530

    Project-5, keyboardCheck

---
+++ Javascript/Projects/Project-5_keyboardCheck/script.js
@@ -0,0 +1,33 @@
+const insert = document.getElementById('insert');
+
+window.addEventListener('keydown', (e) => {
+  insert.innerHTML = `
+    <div class='color'>
+    <table>
+    <tr>
+      <th>Key</th>
+      <th>Keycode</th> 
+      <th>Code</th>
+    </tr>
+    <tr>
+      <td>${e.key === ' ' ? 'Space' : e.key}</td>
+      <td>${e.keyCode}</td> 
+      <td>${e.code}</td>
+    </tr>
+    
+  </table>
+    </div>
+  `;
+});
+
+
+
+//------------------------------------------------------------
+// window.addEventListener('keydown',function(e){
+//     insert.innerHTML = `<h2>${e.key}</h2>`
+// })
+
+// window.addEventListener('keydown',function(e){
+//     insert.innerHTML += e.key
+// })
+
