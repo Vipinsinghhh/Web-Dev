1ca5380ec84b1956e49cfbabf32d6f9151292379
Vipinsinghhh <169113380+Vipinsinghhh@users.noreply.github.com>
2025-12-28 02:15:08 +0530

    Created counter component with increment and decrement buttons using hooks

---
+++ React/02Counter/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setcounter] = useState(15)
+
+  //let counter = 15
+
+  const addValue=()=>{
+    if(counter === 20){
+      return;
+    }
+    setcounter(counter+1)
+  }
+
+  const removeValue=()=>{
+    if(counter === 0){
+      return;
+    }
+    setcounter(counter-1)
+  }
+  
+  return (
+    <>
+    <h1>Counter Project</h1>
+    <h2>Counter Value: {counter}</h2>
+
+    <button onClick={addValue}>Add Value {counter}</button>
+    <br/>
+    <button onClick={removeValue}>remove Value {counter}</button>
+    <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
