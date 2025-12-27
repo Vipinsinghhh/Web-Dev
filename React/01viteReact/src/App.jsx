992d2a3afad330de262c51d7ec2a221310f94a98
Vipinsinghhh <169113380+Vipinsinghhh@users.noreply.github.com>
2025-12-27 22:22:10 +0530

    custom react added

--- React/01viteReact/src/App.jsx
+++ React/01viteReact/src/App.jsx
@@ -1,10 +1,12 @@
 import Hello from "./New"
 
 function App() {
+  const username = "chai aur code"
   return (
     <>
     <Hello/>
-    <h1>Hello World with vite react</h1>
+    <h1>Hello World with vite react, {username}</h1>           //just like that we insert variables in react inside the curly braces{}
+    <p>test para</p>
     </>
   )
 }
--- React/01viteReact/src/main.jsx
+++ React/01viteReact/src/main.jsx
@@ -1,10 +1,40 @@
-import { StrictMode } from 'react'
+import React,{ StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
 
+function MyApp(){              //hum yha bhi function bna skte he
+  return(
+    <div>
+      <h1>custom App | chai</h1>
+    </div>
+  )
+}
+
+// const ReactElement = {                 
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google'
+// }
+
+const anotherElement = (                                                   //jsx se element create krna
+    <a href="https://google.com" target='_blank'>Visit google </a>
+)
+
+const anotherUser = " chai aur react"
+
+const reactElement = React.createElement(               //React method se element create krna      
+    'a',
+    {href: 'https://google.com',target: '_blank'},
+    'click me to visit google',
+    anotherUser   //yha curly braces nhi lgte variable par
+)
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+  {reactElement}
+  </StrictMode>
 )
